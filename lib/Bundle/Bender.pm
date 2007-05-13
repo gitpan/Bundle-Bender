5728d82e9c98c4b8fda4f0caf821375cc61f639b
Chris Prather <chris@prather.org>
2007-05-13 20:41:02 +0000

    Import of PERIGRIN/Bundle-Bender-1.0 from CPAN.
    gitpan-cpan-distribution: Bundle-Bender
    gitpan-cpan-version:      1.0
    gitpan-cpan-path:         PERIGRIN/Bundle-Bender-1.0.tar.gz
    gitpan-cpan-author:       PERIGRIN
    gitpan-cpan-maturity:     released

---
+++ lib/Bundle/Bender.pm
@@ -0,0 +1,49 @@
+package Bundle::Bender;
+use strict;
+use vars qw($VERSION);
+
+$VERSION = '1.0';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::Bender - Bundle to install all Bender related Modules
+
+=head1 SYNOPSIS
+
+    perl -MCPANPLUS -e 'install Bundle::Bender'
+
+=head1 DESCRIPTION
+
+Bundle::Bender - Bundle to install all Bender's dependencies.
+
+=head1 CONTENTS
+
+POE 0.9989
+
+Moose 0.20
+
+MooseX::Storage 0.01
+
+POE::Component::IRC 5.30
+
+Module::Pluggable::Dependency 0.0.3
+
+Regexp::Common::IRC 0.02
+
+POE::Component::Generic 
+
+Net::AIML 0.01
+
+HTML::Strip 1.06
+
+Bone::Easy 0.04
+
+=head1 AUTHOR
+
+Chris Williams <chris@bingosnet.co.uk>
+
+=cut
